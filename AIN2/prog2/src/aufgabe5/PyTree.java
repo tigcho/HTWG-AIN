88a2d3642ee73dddcd5fde978fed1cb5f34036fb
tigcho <119869553+tigcho@users.noreply.github.com>
2023-11-28 21:06:46 +0100

    Add files via upload

--- AIN2/prog2/src/aufgabe5/PyTree.java
+++ AIN2/prog2/src/aufgabe5/PyTree.java
@@ -3,28 +3,27 @@ package aufgabe5;
 public class PyTree {
 
     private static final double min = 0.001;
+    private static final double angle = Math.toRadians(30);
 
     public static void main(String[] args) {
         double x = 0.6;
-        double y = 0.05;
+        double y = 0.05; // Punkt A Koordinaten
 
         StdDraw.show(0);
         StdDraw.setCanvasSize(800, 800);
-        StdDraw.clear(StdDraw.BLACK);
-        StdDraw.setPenRadius(0.002);
-        PyTree1(x, y, 0, 0.15);
-        // PyTree2(x, y, 0, 0.10);
+        StdDraw.clear(StdDraw.BOOK_BLUE);
+        StdDraw.setPenRadius(0.003);
+        // PyTreeOne(x, y, 0, 0.15);
+        PyTreeTwo(x, y, 0, 0.09);
         StdDraw.show(0);
     }
 
-    static void PyTree1(double x, double y, double alpha, double size) {
+    static void PyTreeOne(double x, double y, double alpha, double size) {
         if (size < min) {
             StdDraw.setPenColor(StdDraw.GREEN);
         } else {
             StdDraw.setPenColor(StdDraw.ORANGE);
         }
-
-        double delta = 0.5;
 
         double s = size * Math.sin(alpha);
         double c = size * Math.cos(alpha);
@@ -38,26 +37,26 @@ public class PyTree {
         double xD = x - s;
         double yD = y + c;
 
-        double u = size * Math.cos(delta);
-        double v = size * Math.sin(delta);
-        double xE = xD + u * Math.cos(alpha + delta);
-        double yE = yD + u * Math.sin(alpha + delta);
+        double u = size * Math.cos(angle);
+        double v = size * Math.sin(angle);
 
-        // Drawing Rectangles
+        double xE = xD + u * Math.cos(alpha + angle);
+        double yE = yD + u * Math.sin(alpha + angle);
+
         // StdDraw.line(x, y, xB, yB); // A -> B
         StdDraw.line(xB, yB, xC, yC); // B -> C
         // StdDraw.line(xC, yC, xD, yD); // C-> D
         StdDraw.line(xD, yD, x, y); // D -> A
 
         if (min < size) {
-            PyTree1(xD, yD, alpha + delta, u);
-            PyTree1(xE, yE, alpha + delta - 1.5, v);
+            PyTreeOne(xD, yD, alpha + angle, u);
+            PyTreeOne(xE, yE, alpha + angle - 1.5, v);
         }
     }
 
-    static void PyTree2(double x, double y, double alpha, double size) {
+    static void PyTreeTwo(double x, double y, double alpha, double size) {
         double height = Math.random() * size + size;
-        double beta = Math.toRadians(Math.random() * 45 + 10);
+        double delta = Math.toRadians(Math.random() * 40 + 10);
 
         if (size < min) {
             StdDraw.setPenColor(StdDraw.GREEN);
@@ -67,6 +66,7 @@ public class PyTree {
 
         double s = size * Math.sin(alpha);
         double c = size * Math.cos(alpha);
+
         double r = height * Math.sin(alpha);
         double h = height * Math.cos(alpha);
 
@@ -79,10 +79,10 @@ public class PyTree {
         double xD = x - r;
         double yD = y + h;
 
-        double u = size * Math.cos(beta);
-        double v = size * Math.sin(beta);
-        double xE = xD + u * Math.cos(alpha + beta);
-        double yE = yD + u * Math.sin(alpha + beta);
+        double u = size * Math.cos(delta);
+        double v = size * Math.sin(delta);
+        double xE = xD + u * Math.cos(alpha + delta);
+        double yE = yD + u * Math.sin(alpha + delta);
 
         // Drawing Rectangles
         // StdDraw.line(x, y, xB, yB); // A -> B
@@ -91,8 +91,8 @@ public class PyTree {
         StdDraw.line(xD, yD, x, y); // D-> A
 
         if (min < size) {
-            PyTree2(xD, yD, alpha + beta, u);
-            PyTree2(xE, yE, alpha + beta - 1.5, v);
+            PyTreeTwo(xD, yD, alpha + delta, u);
+            PyTreeTwo(xE, yE, alpha + delta - 1.5, v);
         }
     }
 }
