db544ec01e41c1492cfef59878ec1c2c49f88acd
tigcho <119869553+tigcho@users.noreply.github.com>
2023-12-22 14:57:02 +0100

    Add files via upload

--- AIN2/sysprog/aufgabe5/benotung.cpp
+++ AIN2/sysprog/aufgabe5/benotung.cpp
@@ -1,10 +1,13 @@
 #include "benotung.h"
 #include <stdexcept>
 
-benotung benotung::beste = benotung(10);
-benotung benotung::schlechteste = benotung(50);
+const benotung benotung::beste = benotung(10);
+const benotung benotung::schlechteste = benotung(50);
 
+// TODO: öffentlichen expliziten Konstruktor benotung(int)
 benotung::benotung(int n)    : note(n) {
+
+// TODO: Werfen Sie bei unzulässiger Note eine Ausnahme vom Typ std::invalid_argument
     if (note != 10 && note != 13 && note != 17 && note != 20 && note != 23 &&
         note != 27 && note != 30 && note != 33 && note != 37 && note != 40 &&
         note != 50) {
@@ -12,14 +15,16 @@ benotung::benotung(int n)    : note(n) {
     }
 }
 
-int benotung::int_value() {
+int benotung::int_value() const {
     return note;
 }
 
-bool benotung::ist_bestanden() {
+// TODO: ...die true liefert, wenn der Wert der gekapselten Note kleiner oder gleich 40 ist
+bool benotung::ist_bestanden() const {
     return note <= 40;
 }
 
-bool operator==(benotung &a, benotung &b) {
-    return a.note == b.note;
+// TODO: ...die true liefert, wenn beide Objekte die gleiche Noten kapseln
+bool operator==(const benotung &a, const benotung &b) {
+    return a.int_value() == b.int_value();
 }
--- AIN2/sysprog/aufgabe5/fachnoten_liste.h
+++ AIN2/sysprog/aufgabe5/fachnoten_liste.h
@@ -1,40 +1,41 @@
+
 #ifndef FACHNOTEN_LISTE_H
 #define FACHNOTEN_LISTE_H
 
 #include "fachnote.h"
 
-class fachnoten_liste
-{
-fachnoten_liste(fachnoten_liste&) = delete;
-fachnoten_liste& operator=(fachnoten_liste&) = delete;
-fachnoten_liste(fachnoten_liste&&) = delete;
-fachnoten_liste& operator=(fachnoten_liste&&) = delete;
+class fachnoten_liste {
+
 
 private:
-class element;
-element *head;
-void (*function)(fachnote*);
+    class element;
+    element *head;
+    void (*function)(fachnote*);
 
 public:
-explicit fachnoten_liste(void (*f)(fachnote*));
-~fachnoten_liste();
-fachnoten_liste& insert(fachnote*);
+    fachnoten_liste(const fachnoten_liste&) = delete;
+    fachnoten_liste& operator=(fachnoten_liste&) = delete;
+    fachnoten_liste(const fachnoten_liste&&) = delete;
+    fachnoten_liste& operator=(fachnoten_liste&&) = delete;
 
-class iterator final
-{
-private:
-element *current;
-explicit iterator(element*);
-public:
-bool operator!=(const iterator&) const;
-fachnote* operator*() const;
-iterator& operator++();
+    explicit fachnoten_liste(void (*f)(fachnote*));
+    ~fachnoten_liste();
+    fachnoten_liste& insert(fachnote*);
 
-friend class fachnoten_liste;
-};
+    class iterator final {
+    private:
+        element *current;
+        explicit iterator(element*);
+    public:
+        bool operator!=(const iterator&) const;
+        fachnote* operator*() const;
+        iterator& operator++();
 
-iterator begin();
-iterator end();
+        friend class fachnoten_liste;
+    };
+
+    iterator begin();
+    iterator end();
 };
 
 #endif
