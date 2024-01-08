7a07ee07a91fcf49de146830d79603d51bea556c
tigcho <119869553+tigcho@users.noreply.github.com>
2024-01-09 00:12:18 +0100

    kp was abgeht

---
+++ AIN2/prog2/src/aufgabe8/Taschenrechner.java
@@ -0,0 +1,16 @@
+package aufgabe8;
+
+import javax.swing.*;
+
+public class Taschenrechner {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Taschenrechner");
+        JPanel panel = new GUI().mainPanel;
+        frame.setContentPane(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(400, 500);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+}
