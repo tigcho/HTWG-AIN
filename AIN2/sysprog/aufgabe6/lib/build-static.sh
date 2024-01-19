846c497594d2edac65b78537827449aad81b80ab
tigcho <119869553+tigcho@users.noreply.github.com>
2024-01-19 17:18:56 +0000

    yippiee! mc dragon hat es bestätigt!

--- AIN2/sysprog/aufgabe6/lib/build-static.sh
+++ AIN2/sysprog/aufgabe6/lib/build-static.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 for s in benotung fachnote fachnoten_liste ; do
-  command="g++ -g -c -Wall -Wextra -Werror -std=c++11 -pedantic $s.pp"
+  command="g++ -g -c -Wall -Wextra -Werror -std=c++11 -pedantic $s.cpp"
+  echo "Source File: $s.cpp"
   echo $command
   eval $command
   if [ $? -ne 0 ] ; then
