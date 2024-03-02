f7146b5b193bf624b7d62291bca2180dbf080948
Selin K <selin@yui.hachiko>
2024-03-02 16:27:55 +0100

    blub

---
+++ AIN3/alda/aufgabe2/graph/StrongComponents.java
@@ -0,0 +1,78 @@
+// O. Bittel;
+// 22.02.2017
+
+package directedGraph;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
+
+/**
+ * Klasse für Bestimmung aller strengen Komponenten.
+ * Kosaraju-Sharir Algorithmus.
+ * @author Oliver Bittel
+ * @since 22.02.2017
+ * @param <V> Knotentyp.
+ */
+public class StrongComponents<V> {
+	// comp speichert jede Komponente die zughörigen Knoten. 
+	private final Map<Integer,Set<V>> comp = new TreeMap<>();
+	
+	// Anzahl der Komponenten:
+	private int numberOfComp = 0;
+	
+	/**
+	 * Ermittelt alle strengen Komponenten mit
+	 * dem Kosaraju-Sharir Algorithmus.
+	 * @param g gerichteter Graph.
+	 */
+	public StrongComponents(DirectedGraph<V> g) {
+		// ...
+	}
+	
+	/**
+	 * 
+	 * @return Anzahl der strengen Komponeneten.
+	 */
+	public int numberOfComp() {
+		return numberOfComp;
+	}
+
+	@Override
+	public String toString() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+	
+		
+	public static void main(String[] args) {
+		DirectedGraph<Integer> g = new AdjacencyListDirectedGraph<>();
+		g.addEdge(1,2);
+		g.addEdge(1,3);
+		g.addEdge(2,1);
+		g.addEdge(2,3);
+		g.addEdge(3,1);
+		
+		g.addEdge(1,4);
+		g.addEdge(5,4);
+		
+		g.addEdge(5,7);
+		g.addEdge(6,5);
+		g.addEdge(7,6);
+		
+		g.addEdge(7,8);
+		g.addEdge(8,2);
+		
+		StrongComponents<Integer> sc = new StrongComponents<>(g);
+		
+		System.out.println(sc.numberOfComp());  // 4
+		
+		System.out.println(sc);
+			// Component 0: 5, 6, 7, 
+        	// Component 1: 8, 
+            // Component 2: 1, 2, 3, 
+            // Component 3: 4, 
+	}
+}
