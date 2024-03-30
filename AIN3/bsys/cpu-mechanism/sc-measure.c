4f903a2de252ea433a2938602ff18a1bfe2a53ac
selin <selinkabak@protonmail.com>
2024-03-30 16:45:00 +0100

    .

--- AIN3/bsys/cpu-mechanism/sc-measure.c
+++ AIN3/bsys/cpu-mechanism/sc-measure.c
@@ -1,16 +1,19 @@
 #include <sys/time.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 #define ITER 1000000
 
 int main() {
     struct timeval tv;
+   int fd = open("./1.txt", O_CREAT | O_RDONLY, S_IRWXU), nloops = 1000000;
+
     gettimeofday(&tv, NULL);
     double start = tv.tv_sec + (tv.tv_usec / 1000000.0);
     for (int i = 0; i < ITER; i++)
     {
-        read(0, NULL, 0);
+      read(fd, NULL, 0);
     }
     gettimeofday(&tv, NULL);
     double end = tv.tv_sec + (tv.tv_usec / 1000000.0);
@@ -19,5 +22,5 @@ int main() {
         1000 * (end - start) / (double)ITER,
         1000000 * (end - start) / (double)ITER);
 
-    return 0;
+    close(fd);   return 0;
 }
