bf0f10fef4b2d6d4a9a6703240b2b9848a9a9189
selin <selinkabak@protonmail.com>
2024-06-27 19:23:47 +0200

    booking

--- AIN3/dbs/sql/Select.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM dbsys34.adresse WHERE ADRESS_ID = 1;
-SELECT * FROM dbsys34.adresse WHERE ADRESS_ID = 2;
-SELECT * FROM dbsys34.ferienwohnung w, dbsys34.beinhaltete_ausstattung baus, dbsys34.ausstattung aus
-    WHERE w.WOHNUNGS_ID = 1 AND baus.WOHNUNGS_ID = w.WOHNUNGS_ID AND aus.AUSSTATTUNGSNAME = baus.AUSSTATTUNGSNAME;
-SELECT * FROM dbsys34.bild b, dbsys34.ferienwohnung f WHERE f.WOHNUNGS_ID = 1 AND f.WOHNUNGS_ID = b.wohnungs_id;
-SELECT attr.* FROM dbsys34.ferienwohnung f, dbsys34.attraktion attr, dbsys34.attraktions_entfernung ae
-    WHERE f.WOHNUNGS_ID = 1 AND f.WOHNUNGS_ID = ae.WOHNUNGS_ID AND attr.ATTRAKTIONSNAME = ae.ATTRAKTIONSNAME;
-SELECT * FROM dbsys34.buchung WHERE buchungsnummer = 1;
-SELECT * FROM dbsys34.kunde WHERE mail = 'mustermann@muster.com';
