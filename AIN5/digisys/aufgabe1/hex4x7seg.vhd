c951653f0875721034f56eb2dc441b4bc4e27612
selin <selinkabak@protonmail.com>
2024-10-26 17:30:03 +0200

    digisys ex1 part 2

--- AIN5/digisys/aufgabe1/hex4x7seg.vhd
+++ AIN5/digisys/aufgabe1/hex4x7seg.vhd
@@ -1,3 +1,4 @@
+
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.std_logic_unsigned.ALL;
@@ -14,62 +15,89 @@ ENTITY hex4x7seg IS
 END hex4x7seg;
 
 ARCHITECTURE struktur OF hex4x7seg IS
-   CONSTANT reg: std_logic_vector := "11111111111111"; -- initial value for the counter 
-   SIGNAL cnt: std_logic_vector(13 DOWNTO 0);          -- modulo-2**14 counter (16383)
-   SIGNAL en: std_logic := '0';                        -- enable signal for the 4-digit display
-
-   CONSTANT N: integer := 4;
-   SIGNAL cnt4: integer RANGE 0 TO N-1 := 0;           -- modulo-4 counter
+   SIGNAL counter: std_logic_vector(13 DOWNTO 0) := (OTHERS => '0');
+   SIGNAL mod4_counter: std_logic_vector(1 DOWNTO 0) := (OTHERS => '0');
+   SIGNAL phase: std_logic_vector(3 DOWNTO 0) := (OTHERS => '0');
+   SIGNAL mux_out: std_logic_vector(3 DOWNTO 0);
+   SIGNAL seg_out: std_logic_vector(7 DOWNTO 1);
 BEGIN
 
    -- Modulo-2**14-Zaehler
-   -- use LFSR like in lecture slides
-   p1: PROCESS(rst, clk) IS
-   BEGIN
-      IF rst = RSTDEF THEN
-         cnt <= (OTHERS => '0');
-         en <= '0';
-      ELSIF rising_edge(clk) THEN
-         IF cnt = (OTHERS => '0') THEN
-            cnt <= reg;
-         ELSE
-
-            -- Polynom: x^14 + x^13 + x^12 + x^2 + 1 
-            -- shift the entire register by one position to the right
-            cnt(13 DOWNTO 1) <= cnt(12 DOWNTO 0);
-            -- calculate the new value for the leftmost bit
-            cnt(0) <= cnt(13) XOR cnt(12) XOR cnt(11) XOR cnt(1);
-
-         if cnt = reg THEN
-            en <= '1';
-         ELSE
-            en <= '0';
-         END IF;
-      END IF;
-   END PROCESS p1;
+   process(clk, rst)
+   begin
+      if rst = RSTDEF then
+         counter <= (OTHERS => '0');
+      elsif rising_edge(clk) then
+         if counter = "11111111111111" then
+            counter <= (OTHERS => '0');
+         else
+            counter <= counter + 1;
+         end if;
+      end if;
+   end process;
    
    -- Modulo-4-Zaehler
-   p2: PROCESS(rst, clk) IS
-   BEGIN
-      IF rst = RSTDEF THEN
-         cnt4 <= 0;
-      ELSIF rising_edge(clk) THEN -- clk switches from 0 to 1
-         IF en = '1' THEN         -- only keep counting if en is set
-            IF cnt = N-1 THEN     -- if cnt is 3, reset 
-               cnt <= 0;
-            ELSE
-               cnt <= cnt + 1;
-            END IF;
-         END IF;
-      END IF;
-   END PROCESS p2;
+   process(clk, rst)
+   begin
+      if rst = RSTDEF then
+         mod4_counter <= (OTHERS => '0');
+      elsif rising_edge(clk) then
+         if mod4_counter = "11" then
+            mod4_counter <= (OTHERS => '0');
+         else
+            mod4_counter <= mod4_counter + 1;
+         end if;
+      end if;
+   end process;
 
    -- 1-aus-4-Dekoder als Phasengenerator
+   process(mod4_counter)
+   begin
+      case mod4_counter is
+         when "00" => phase <= "0001";
+         when "01" => phase <= "0010";
+         when "10" => phase <= "0100";
+         when "11" => phase <= "1000";
+         when others => phase <= "0000";
+      end case;
+   end process;
        
    -- 1-aus-4-Multiplexer
+   process(mod4_counter, data)
+   begin
+      case mod4_counter is
+         when "00" => mux_out <= data(3 DOWNTO 0);
+         when "01" => mux_out <= data(7 DOWNTO 4);
+         when "10" => mux_out <= data(11 DOWNTO 8);
+         when "11" => mux_out <= data(15 DOWNTO 12);
+         when others => mux_out <= (OTHERS => '0');
+      end case;
+   end process;
 
    -- 7-aus-4-Dekoder
+   process(mux_out)
+   begin
+      case mux_out is
+         when "0000" => seg_out <= "1111110"; -- 0
+         when "0001" => seg_out <= "0110000"; -- 1
+         when "0010" => seg_out <= "1101101"; -- 2
+         when "0011" => seg_out <= "1111001"; -- 3
+         when "0100" => seg_out <= "0110011"; -- 4
+         when "0101" => seg_out <= "1011011"; -- 5
+         when "0110" => seg_out <= "1011111"; -- 6
+         when "0111" => seg_out <= "1110000"; -- 7
+         when "1000" => seg_out <= "1111111"; -- 8
+         when "1001" => seg_out <= "1111011"; -- 9
+         when "1010" => seg_out <= "1110111"; -- A
+         when "1011" => seg_out <= "0011111"; -- b
+         when "1100" => seg_out <= "1001110"; -- C
+         when "1101" => seg_out <= "0111101"; -- d
+         when "1110" => seg_out <= "1001111"; -- E
+         when "1111" => seg_out <= "1000111"; -- F
+         when others => seg_out <= "0000000"; -- default
+      end case;
+   end process;
 
-   -- 1-aus-4-Multiplexer
+   seg <= seg_out;
 
 END struktur;
