dcaea8a0a52c5b35b729aaffe0b85c10b0ac90d7
selin <selinkabak@protonmail.com>
2024-12-11 16:40:02 +0100

    digisys done

--- AIN5/digisys/aufgabe1/hex4x7seg.vhd
+++ AIN5/digisys/aufgabe1/hex4x7seg.vhd
@@ -15,13 +15,35 @@ END hex4x7seg;
 
 ARCHITECTURE struktur OF hex4x7seg IS
    SIGNAL counter: std_logic_vector(13 DOWNTO 0);
-   SIGNAL mod4_counter: std_logic_vector(1 DOWNTO 0);
+--   SIGNAL mod4_counter: std_logic_vector(1 DOWNTO 0);
    SIGNAL phase: std_logic_vector(3 DOWNTO 0);
    SIGNAL mux_out: std_logic_vector(3 DOWNTO 0);
    SIGNAL seg_out: std_logic_vector(7 DOWNTO 1);
    SIGNAL dp_out: std_logic_vector(3 DOWNTO 0);
    SIGNAL en: std_logic;
+   SIGNAL carry_out: std_logic;
+   signal prev_carry_out: std_logic;
    CONSTANT N: integer := 4;
+   TYPE segment_table is array (0 to 15) of std_logic_vector(7 downto 1);
+   constant SEGMENTS: segment_table := (
+      "0111111", -- 0
+      "0000110", -- 1
+      "1011011", -- 2
+      "1001111", -- 3
+      "1100110", -- 4
+      "1101101", -- 5
+      "1111101", -- 6
+      "0000111", -- 7
+      "1111111", -- 8
+      "1101111", -- 9
+      "1110111", -- A
+      "1111100", -- b
+      "0111001", -- C
+      "1011110", -- d
+      "1111001", -- E
+      "1110001"  -- F
+   );
+
 BEGIN
 
    -- Modulo-2**14-Zaehler
@@ -29,110 +51,76 @@ BEGIN
    begin
       if rst = RSTDEF then
          counter <= (OTHERS => '0');
+         carry_out <= '0';
       elsif rising_edge(clk) then
-         if counter = "11111111111111" then
-            en <= '1';
-            counter <= (OTHERS => '0');
-         else
-            en <= '0';
-            counter <= std_logic_vector(unsigned(counter) + 1);
+         counter <= std_logic_vector(unsigned(counter) + 1);
+         carry_out <= counter(13);
+         prev_carry_out <= carry_out;
+      end if;
+   end process;
+
+   en <= carry_out and not prev_carry_out; -- Use the MSB as enable signal
+   
+   -- Modulo-4-Zaehler
+--   process(clk, rst)
+--   begin
+--      if rst = RSTDEF then
+--         mod4_counter <= "00";
+--      elsif rising_edge(clk) then
+--         mod4_counter <= std_logic_vector(unsigned(mod4_counter) + 1);
+--      end if;
+--   end process;
+
+
+   -- 1-aus-4-Dekoder als Phasengenerator
+   process(clk, rst)
+   begin
+      if rst = RSTDEF then
+         phase <= "0000"; -- No digit lit during reset
+      elsif rising_edge(clk) then
+         if en = '1' then
+            if phase = "0000" then
+               phase <= "0001"; -- Start cycling when enabled
+            else
+               phase <= phase(2 downto 0) & phase(3); -- Rotate left
+            end if;
          end if;
       end if;
    end process;
-   
-   -- Modulo-4-Zaehler
-   p2: PROCESS(rst, en) IS
-   BEGIN
-      IF rst = RSTDEF THEN
-         mod4_counter <= (OTHERS => '0');
-      ELSIF rising_edge(en) THEN -- clk switches from 0 to 1
-         IF mod4_counter = "11" THEN     -- if cnt is 3, reset 
-            mod4_counter <= (OTHERS => '0');
-         ELSE
-            mod4_counter <= std_logic_vector(unsigned(mod4_counter) + 1);
-         END IF;
-      END IF;
-   END PROCESS p2;
 
-   -- 1-aus-4-Dekoder als Phasengenerator
-   process(mod4_counter)
-   begin
-      case mod4_counter is
-         when "00" => phase <= "0001";
-         when "01" => phase <= "0010";
-         when "10" => phase <= "0100";
-         when "11" => phase <= "1000";
-         when others => phase <= "0000";
-      end case;
-   end process;
        
    -- 1-aus-4-Multiplexer
-   process(mod4_counter, data)
-   begin
-      case mod4_counter is
-         when "00" => mux_out <= data(3 DOWNTO 0);
-         when "01" => mux_out <= data(7 DOWNTO 4);
-         when "10" => mux_out <= data(11 DOWNTO 8);
-         when "11" => mux_out <= data(15 DOWNTO 12);
-         when others => mux_out <= (OTHERS => '0');
-      end case;
-   end process;
+   mux_out <= data(3 downto 0) when phase = "0001" else
+      data(7 downto 4) when phase = "0010" else
+      data(11 downto 8) when phase = "0100" else
+      data(15 downto 12) when phase = "1000" else
+      (OTHERS => '0');
+
+   dp <= dp_out(0) when phase = "0001" else
+      dp_out(1) when phase = "0010" else
+      dp_out(2) when phase = "0100" else
+      dp_out(3) when phase = "1000" else
+      '0';
       
    -- 7-aus-4-Dekoder
-   process(mux_out, rst)
+   process(mux_out)
    begin
-      if rst = RSTDEF then
-         seg_out <= "0000000";
-      else
-         case mux_out is
-            when "0000" => seg_out <= "0111111"; -- 0
-            when "0001" => seg_out <= "0000110"; -- 1
-            when "0010" => seg_out <= "1011011"; -- 2
-            when "0011" => seg_out <= "1001111"; -- 3
-            when "0100" => seg_out <= "1100110"; -- 4
-            when "0101" => seg_out <= "1101101"; -- 5
-            when "0110" => seg_out <= "1111101"; -- 6
-            when "0111" => seg_out <= "0000111"; -- 7
-            when "1000" => seg_out <= "1111111"; -- 8
-            when "1001" => seg_out <= "1101111"; -- 9
-            when "1010" => seg_out <= "1110111"; -- A
-            when "1011" => seg_out <= "1111100"; -- b
-            when "1100" => seg_out <= "0111001"; -- C
-            when "1101" => seg_out <= "1011110"; -- d
-            when "1110" => seg_out <= "1111001"; -- E
-            when "1111" => seg_out <= "1110001"; -- F
-            when others => seg_out <= "0000000"; -- default
-         end case;
-      end if;
+      seg_out <= SEGMENTS(to_integer(unsigned(mux_out)));
    end process;
 
    -- Decimal Point
-   process(dpin)
-   begin
-      if rising_edge(dpin(0)) then
-         dp_out(0) <= dp_out(0) xor '1';
-      end if;
-      if rising_edge(dpin(1)) then
-         dp_out(1) <= dp_out(1) xor '1';
-      end if;
-      if rising_edge(dpin(2)) then
-         dp_out(2) <= dp_out(2) xor '1';
-      end if;
-      if rising_edge(dpin(3)) then
-         dp_out(3) <= dp_out(3) xor '1';
-      end if;
-   end process;
+--  process(clk)
+--   begin
+--      for i in 0 to 3 loop
+--         if dpin(i) = '1' then
+--            dp_out(i) <= '1';
+--         else
+--            dp_out(i) <= '0';
+--         end if;
+--      end loop;
+--   end process;
 
-   process(mod4_counter, dp_out)
-   begin
-      case mod4_counter is
-         when "00" => dp <= dp_out(0);
-         when "01" => dp <= dp_out(1);
-         when "10" => dp <= dp_out(2);
-         when "11" => dp <= dp_out(3);
-         when others => dp <= '0';
-      end case;
-   end process;
+   dp_out <= dpin;
             
    ena <= phase;
    seg <= seg_out;
